da541be0e307f129d69cc0312c00b78ddddbc872
Choi Yuri <yurifrom1123@gmail.com>
2023-10-16 10:43:25 +0900

    - mybatis 연결 - 게시판 domain, dao, service, controller 생성 - 게시판 리스트 생성

---
+++ src/main/java/com/yuls/yspringboot1/Yspringboot1Application.java
@@ -0,0 +1,13 @@
+package com.yuls.yspringboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yspringboot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Yspringboot1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuls/yspringboot1/Yspringboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuls.yspringboot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yspringboot1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
