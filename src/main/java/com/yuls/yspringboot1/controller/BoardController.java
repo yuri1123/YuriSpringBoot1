bba8f1236700a44d214543112c7aa52b744ecc03
Choi Yuri <yurifrom1123@gmail.com>
2023-10-30 14:47:05 +0900

    - 접근권한에 따른 메시지 출력

--- src/main/java/com/yuls/yspringboot1/controller/BoardController.java
+++ src/main/java/com/yuls/yspringboot1/controller/BoardController.java
@@ -1,13 +1,14 @@
 package com.yuls.yspringboot1.controller;
 
-import com.yuls.yspringboot1.dto.BoardDto;
-import com.yuls.yspringboot1.dto.PagingResponse;
-import com.yuls.yspringboot1.dto.SearchDto;
+import com.yuls.yspringboot1.dto.*;
 import com.yuls.yspringboot1.service.BoardService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.HashMap;
 import java.util.List;
@@ -21,29 +22,40 @@ public class BoardController {
 
     //게시판 리스트 페이지 가기
     @GetMapping("/board")
-    public String goboard(Model model, @ModelAttribute("params") final SearchDto params){
+    public String goboard(Model model, @ModelAttribute("params") final SearchDto params) {
         System.out.println("게시판으로 가는 길");
         PagingResponse<BoardDto> response = boardService.findAllPost(params);
 //        List<BoardDto> boardList = boardService.selectboard(params);
         System.out.println(response);
-        model.addAttribute("response",response);
+        model.addAttribute("response", response);
 
         return "board";
     }
 
+
     //글쓰기 페이지 가기
     @GetMapping("/write")
-    public String gowriteboard(Model model){
-        System.out.println("글쓰기가기");
-        return "createboard";
+    public String gowriteboard(HttpSession session, RedirectAttributes attributes) {
+        Object user = session.getAttribute("user");
+        if (user == null) {
+            System.out.println("로그인 정보가 없다고! ");
+            // 세션 정보가 없을 때 메시지와 리디렉션 정보를 추가합니다.
+            attributes.addFlashAttribute("data", new MessageDto("접근권한이 없습니다.", "/login"));
+            return "redirect:/board"; // board 컨트롤러로 리디렉션합니다.
+        } else {
+            System.out.println("설마 로그인정보가 없는데 넘어가고 있니?");
+            // 세션 정보가 있는 경우 글쓰기 페이지로 이동
+            return "createboard";
+        }
     }
 
     //글쓰기
     @PostMapping("/createboard")
-    public String writeboard(Model model,BoardDto boardDto){
-        if(boardDto != null){
+    public String writeboard(Model model, BoardDto boardDto) {
+        if (boardDto != null) {
             int result = boardService.create(boardDto);
         }
+
 //        List<BoardDto> boardList = boardService.selectboard();
 //        model.addAttribute("boardList", boardList);
 
@@ -52,22 +64,22 @@ public class BoardController {
 
     //게시글 보기(id로 게시물 조회)
     @GetMapping("/boardview/")
-    public String boardview(@RequestParam final Long id,final SearchDto queryParams, BoardDto boardDto, Model model){
+    public String boardview(@RequestParam final Long id, final SearchDto queryParams, BoardDto boardDto, Model model) {
         //게시물 조회
         boardDto = boardService.selectbyid(id);
-        if(boardDto != null) {
+        if (boardDto != null) {
             model.addAttribute("board", boardDto);
         }
         //게시물 viewcnt +1
         int result = boardService.updateviewcnt(id);
-    return "boardview";
+        return "boardview";
     }
 
     //게시글 수정 페이지 가기
     @GetMapping("/updateboard/")
-    public String updateboard(@RequestParam final Long id, BoardDto boardDto, Model model){
+    public String updateboard(@RequestParam final Long id, BoardDto boardDto, Model model) {
         boardDto = boardService.selectbyid(id);
-        if(boardDto != null) {
+        if (boardDto != null) {
             model.addAttribute("board", boardDto);
         }
         return "updateboard";
@@ -75,8 +87,8 @@ public class BoardController {
 
     //게시글 수정
     @PostMapping("/updateboard/{id}")
-    public String updateboarddo(@PathVariable("id") Long id,BoardDto boardDto){
-        if(boardDto != null) {
+    public String updateboarddo(@PathVariable("id") Long id, BoardDto boardDto) {
+        if (boardDto != null) {
             int result = boardService.update(boardDto);
         }
         return "reidrect:/board";
@@ -95,8 +107,8 @@ public class BoardController {
 
     //게시글 삭제
     @PostMapping("/deleteboard/")
-    public String deleteboard(@RequestParam final Long id,final SearchDto queryParams, Model model){
-            boardService.deleteboard(id);
+    public String deleteboard(@RequestParam final Long id, final SearchDto queryParams, Model model) {
+        boardService.deleteboard(id);
 
         return "redirect:/board";
     }
